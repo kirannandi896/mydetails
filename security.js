e9874b7cfba06f5581854d4467f16acfefa7d2c3
kirannandi896 <kirannandi896@gmail.com>
2023-11-20 18:18:44 +0530

    Security fixed1

--- security.js
+++ security.js
@@ -1,10 +1,10 @@
 var s=prompt("Password: ");
 const password = process.env.PASS;
-if(s  ==  password){
+if(s === password){
     alert("Securely opened");
 }
 else
 {
     alert("Wrong password contact me.")
-    window.location="https://kirannandi896.github.io/mydetails/contact.html";
+    window.location="contact.html";
 }
