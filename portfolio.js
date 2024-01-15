d31c65eba3288a182617a57a4f479a1d2cd6043f
kirannandi896 <kirannandi896@gmail.com>
2024-01-15 22:14:14 +0530

    Update2

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+
+function active() {
+    const navLinks = document.querySelectorAll('.overlay-content a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+        navLinks.forEach(link => {
+          link.classList.remove('active');
+        });
+        this.classList.add('active');
+      });
+    });
+}
